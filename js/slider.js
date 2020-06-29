a3d1b2dda99e54573434e2a49a0198747ad754f2
tashasub <clairvoyantfire@yandex.ru>
2020-06-29 21:08:10 +0300

    правки по базовым критериям

--- js/slider.js
+++ js/slider.js
@@ -2,19 +2,19 @@ var sliderControls = document.querySelectorAll(".slider-control");
 var sliderItems = document.querySelectorAll(".slider-item");
 
 sliderControls.forEach(function(sliderControl, i) {
-  if (sliderControls && sliderControls.length) {}
-  sliderControl.addEventListener("click", function() {
+  if (sliderControls && sliderControls.length) {
+    sliderControl.addEventListener("click", function() {
 
-    sliderControls.forEach(function(sliderItemRemove) {
-      sliderItemRemove.classList.remove("current");
+      sliderControls.forEach(function(sliderItemRemove) {
+        sliderItemRemove.classList.remove("current");
+      });
+      sliderControl.classList.add("current");
+
+      sliderItems.forEach(function(sliderItem) {
+        sliderItem.classList.remove("slide-current");
+      });
+
+      sliderItems[i].classList.add("slide-current");
     });
-    sliderControl.classList.add("current");
-
-    sliderItems.forEach(function(sliderItem) {
-      sliderItem.classList.remove("slide-current");
-    });
-
-
-    sliderItems[i].classList.add("slide-current");
-  });
+  }
 });
